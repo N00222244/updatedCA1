94bab2039f8b53548d3e9b069a9ac3c2cac7c70f
N00222244 <N00222244@iadt.ie>
2025-11-10 11:02:41 +0000

    added json transformation the user class to remove sensetive fields, also hashed the password with ten saltrounds and created an instance method to verify the password using bcypts, also added some extra comments througuout the document.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
-import bycrypt from "bycrypt";
+import bcrypt from "bcrypt";
+
+
+
+//defining the user schema, the user object contaiains the fields:
+//Name, Email, Password, Phone number and their Role.
+//Role defaults to user as most users for this will be ordinary users, other roles willl entail escaleted privelages on the server,
+//Editor will be able to edit certain entities details and remove some where as admin will have full crud abilities across the site.
 
 const userSchema = new mongoose.Schema({
     Name:{
@@ -16,11 +23,13 @@ const userSchema = new mongoose.Schema({
 
     Password:{
         type: String,
+        minlength: 5,
         required: true,
     },
     
     Phone:{
         type: "String",
+        minlength: 6,
         required: "false",
     },
 
@@ -37,6 +46,39 @@ const userSchema = new mongoose.Schema({
 }
 
 );
+
+
+// comverting the schema to json and removing certain sensetive fiels such as the object creation/update time records,
+// mongooses versioning, object id and of course the password.
+
+userSchema.set("toJSON", {
+
+    transform: (_document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.Password; 
+        delete returnedObject.createdAt;
+        delete returnedObject.updatedAt;
+    }
+
+})
+
+
+// statically hashinging the pass word with bcrypte and adding 10 different saltrounds to add more uniquness to the hashed password.
+userSchema.statics.hashPassword = async function (password) {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+};
+
+
+
+//this instance  method is used to verify that the password is correct  it uses bcrypts built in compare method to 
+//compare the password vs the passwordhash.
+userSchema.methods.verifyPassword = async function (password) {
+  return await bcrypt.compare(password, this.passwordHash);
+};
+
 
 
 
