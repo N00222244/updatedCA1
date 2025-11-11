b2eac18bded1d39021ebf9c9486bf09e1023868b
N00222244 <N00222244@iadt.ie>
2025-11-11 18:19:07 +0000

    added model schema for dealership

---
+++ models/dealership.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const dealershipSchema = new mongoose.Schema(
+    {
+        dealershipName: {
+            type: String,
+            minlength: 5,
+            required: true,
+        },
+        Location: {
+            type: String,
+            required:true,
+        },
+        Phone: {
+            type: String, 
+            required: true,
+        },
+    }
+)
+
+
+dealershipSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.createdAt;
+    delete returnedObject.updatedAt;
+  },
+});
+
+export default mongoose.model("Dealership", dealershipSchema);
